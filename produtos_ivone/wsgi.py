0ba3f8a1f427f374813887c81110132a1e7430df
Vitor Nere <vitor.nere@hotmail.com>
2017-03-31 12:06:11 -0300

    Projeto iniciado

---
+++ produtos_ivone/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for produtos_ivone project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "produtos_ivone.settings")
+
+application = get_wsgi_application()
