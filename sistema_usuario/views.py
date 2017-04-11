62cc959791be0157901d30977203c363c088272b
Vitor Nere <vitor.nere@hotmail.com>
2017-04-11 00:58:34 -0300

    Finalizado funcionalidade de login

--- sistema_usuario/views.py
+++ sistema_usuario/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate
-from django.contrib.auth import login
+from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
 
@@ -9,10 +9,19 @@ from django.contrib.auth.models import User
 
 def login(request):
 	if request.method == 'POST':
+		# POST
 		usuario = request.POST['usuario']
 		senha = request.POST['senha']
-		user = autenticate(username=usuario, password=senha)
+		user = authenticate(username=usuario, password=senha)
+		if user is not None:
+			# Fazer Login
+			auth_login(request, user)
+			return HttpResponseRedirect('/')
+		else:
+			# Autenticação inválida
+			return render(request, 'sistema_usuario/login.html', {'erro':'Usuário e/ou Senha inválidos.'})
 	else:
+		# GET
 		return render(request, 'sistema_usuario/login.html', {})
 
 
@@ -21,4 +30,4 @@ def signup(request):
 
 
 def logout(request):
-	return HttpResponseRedirect('/index/')
+	return HttpResponseRedirect('/')
