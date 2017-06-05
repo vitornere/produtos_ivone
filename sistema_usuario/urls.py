68c265dcc1a5287b427ff5f6fde0d80bb6a87492
Vitor Nere <vitor.nere@hotmail.com>
2017-06-05 11:03:09 -0300

    Adicionado login dropdown no menu

--- sistema_usuario/urls.py
+++ sistema_usuario/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^login/', views.login, name='login'),
 	url(r'^signup/', views.signup, name='signup'),
 	url(r'^logout/', views.logout, name='logout'),
 ]
