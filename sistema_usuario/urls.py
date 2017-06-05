4b63daae1dd90d68424d74f5a2cbcdd9ad01b2f2
Vitor Nere <vitor.nere@hotmail.com>
2017-06-05 14:38:27 -0300

    Aplicado autopep8

--- sistema_usuario/urls.py
+++ sistema_usuario/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^signup/', views.signup, name='signup'),
-	url(r'^logout/', views.logout, name='logout'),
-	url(r'^login/', views.login, name='login'),
+    url(r'^signup/', views.signup, name='signup'),
+        url(r'^logout/', views.logout, name='logout'),
+        url(r'^login/', views.login, name='login'),
 ]
--- sistema_usuario/views.py
+++ sistema_usuario/views.py
@@ -9,66 +9,66 @@ from django.core.exceptions import ValidationError
 
 # Create your views here.
 
+
 def login(request):
-	if request.method == 'POST':
-		print("Passou")
-		# POST
-		usuario = request.POST['usuario']
-		senha = request.POST['senha']
+    if request.method == 'POST':
+        print("Passou")
+        # POST
+        usuario = request.POST['usuario']
+        senha = request.POST['senha']
 
-		if usuario == "" or senha == "":
-			return render(request, 'sistema_usuario/login.html', {'erro':'Preencha todos os campos.'})#, 'form':form}) #este form não é reconhecido comoum comando válido
+        if usuario == "" or senha == "":
+            return render(request, 'sistema_usuario/login.html', {'erro': 'Preencha todos os campos.'})  # , 'form':form}) #este form não é reconhecido comoum comando válido
 
-		user = authenticate(username=usuario, password=senha)
-		if user is not None:
-			# Fazer Login
-			auth_login(request, user)
-			return HttpResponseRedirect('/')
-		else:
-			# Autenticação inválida
-			return render(request, 'sistema_usuario/login.html', {'erro':'Usuário e/ou Senha inválidos.'})
-	else:
-		# GET
-		return HttpResponseRedirect('/')
+        user = authenticate(username=usuario, password=senha)
+        if user is not None:
+            # Fazer Login
+            auth_login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            # Autenticação inválida
+            return render(request, 'sistema_usuario/login.html', {'erro': 'Usuário e/ou Senha inválidos.'})
+    else:
+        # GET
+        return HttpResponseRedirect('/')
 
 
 def signup(request):
-	if request.method == 'POST':
-		#POST
-		nome = request.POST['nome']
-		sobrenome = request.POST['sobrenome']
-		email = request.POST['email']
-		nome_usuario = request.POST['nome_usuario']
-		senha = request.POST['senha']
-		confirmar_senha = request.POST['confirmar_senha']
+    if request.method == 'POST':
+        # POST
+        nome = request.POST['nome']
+        sobrenome = request.POST['sobrenome']
+        email = request.POST['email']
+        nome_usuario = request.POST['nome_usuario']
+        senha = request.POST['senha']
+        confirmar_senha = request.POST['confirmar_senha']
 
-		if nome == "" or sobrenome == "" or email == "" or nome_usuario == "" or senha == "" or confirmar_senha == "":
-			return render(request, 'sistema_usuario/signup.html', {'erro':'Preencha todos os campos.'})
-		try:
-			validate_email(email)
-		except ValidationError:
-			return render(request, 'sistema_usuario/signup.html', {'erro':'Email inválido'})
-		if senha == confirmar_senha:
-			# Fazer Cadastro
-			user = User.objects.create_user(nome_usuario, email, senha)
-			user.first_name = nome
-			user.last_name = sobrenome
-			user.save()
-			user = authenticate(username=nome_usuario, password=senha)
-			if user is not None:
-				auth_login(request, user)
-				return HttpResponseRedirect('/')
-			else:
-				return render(request, 'sistema_usuario/signup.html', {'erro':'Algo deu muito errado.'})
-		else:
-			return render(request, 'sistema_usuario/signup.html', {'erro':'As senhas não conferem.'})
+        if nome == "" or sobrenome == "" or email == "" or nome_usuario == "" or senha == "" or confirmar_senha == "":
+            return render(request, 'sistema_usuario/signup.html', {'erro': 'Preencha todos os campos.'})
+        try:
+            validate_email(email)
+        except ValidationError:
+            return render(request, 'sistema_usuario/signup.html', {'erro': 'Email inválido'})
+        if senha == confirmar_senha:
+            # Fazer Cadastro
+            user = User.objects.create_user(nome_usuario, email, senha)
+            user.first_name = nome
+            user.last_name = sobrenome
+            user.save()
+            user = authenticate(username=nome_usuario, password=senha)
+            if user is not None:
+                auth_login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                return render(request, 'sistema_usuario/signup.html', {'erro': 'Algo deu muito errado.'})
+        else:
+            return render(request, 'sistema_usuario/signup.html', {'erro': 'As senhas não conferem.'})
 
-
-	else:
-		# GET
-		return render(request, 'sistema_usuario/signup.html', {})
+    else:
+        # GET
+        return render(request, 'sistema_usuario/signup.html', {})
 
 
 def logout(request):
-	auth_logout(request)
-	return HttpResponseRedirect('/')
+    auth_logout(request)
+    return HttpResponseRedirect('/')
