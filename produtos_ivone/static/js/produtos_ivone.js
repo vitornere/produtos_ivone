8c1bc1f1c2925531e462befc23a5dbde66ec242c
Flavio Costa <skingk.f@gmail.com>
2017-06-24 13:38:43 -0300

    Adicionando texto de boas vindas e editando carosel
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>

--- produtos_ivone/static/js/produtos_ivone.js
+++ produtos_ivone/static/js/produtos_ivone.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
-    $('.carousel').carousel();
+	var carousel_interval = 1000;
+	$('.carousel').carousel();
+	var int;
+	function run(){
+	    int = setInterval(function()
+	    { 
+        	$('.carousel').carousel('next');
+	    }, carousel_interval);
+	}
+	function stop(){
+	clearInterval(int);
+	}
+	$('.carousel').hover(stop, run);     
 });
 
 $('#login').webuiPopover({url:'#login-form'});
-
 $(document).ready(function(){
     if($('#erro-login').is(":hidden")) {
         $('#login').click();
